0a4352ea00195d5a6e21b08d93dbaa8ce673f6cf
Zrf <125514209+Ldsqw321@users.noreply.github.com>
2026-05-22 09:37:34 +0300

    14. The System Message

--- index.js
+++ index.js
@@ -11,31 +11,21 @@ checkEnvironment();
 
 const messages = [
   {
-    role: "user",
-    content: `Suggest some gifts for someone who loves hiphop music. 
-    Make these suggestions thoughtful and practical. Your response 
-    must be under 100 words. Skip intros and conclusions. 
+    role: "system",
+    content: `Make these suggestions thoughtful and practical. 
+    Your response must be under 100 words. 
+    Skip intros and conclusions. 
     Only output gift suggestions.`,
+  },
+  {
+    role: "user",
+    content: `Suggest some gifts for someone who loves hiphop music.`,
   },
 ];
 
-const firstResponse = await openai.chat.completions.create({
+const response = await openai.chat.completions.create({
   model: process.env.AI_MODEL,
   messages,
 });
 
-console.log(firstResponse.choices[0].message.content);
-
-messages.push(firstResponse.choices[0].message);
-
-messages.push({
-  role: "user",
-  content: "More budget friendly. Less than $40.",
-});
-
-const secondResponse = await openai.chat.completions.create({
-  model: process.env.AI_MODEL,
-  messages,
-});
-
-console.log(secondResponse.choices[0].message.content);
+console.log(response.choices[0].message.content);
